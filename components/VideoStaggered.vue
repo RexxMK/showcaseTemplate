b5c3f69b20d35a6cdd267b5d89b2938bd4b7a392
RexxMK <rikkekodahl@live.dk>
2024-05-06 14:31:09 +0200

    videoer tilføjet

--- components/VideoStaggered.vue
+++ components/VideoStaggered.vue
@@ -4,12 +4,12 @@
     <div v-if="isMobile">
       <div class="flex justify-center">
         <div
-          v-if="mobile.video"
+          v-if="mobile.src"
           class="bg-amber w-[320px] h-[612px] pt-[10px] pl-[11px]"
         >
           <video
             ref="videoPlayer"
-            :src="mobile.video"
+            :src="mobile.src"
             class="w-[330px] cursor-pointer"
             @click="toggleVideo"
             autoplay
@@ -24,12 +24,12 @@
     <div v-else>
       <div class="flex justify-center">
         <div
-          v-if="laptop.video"
+          v-if="laptop.src"
           class="bg-amber w-[1070px] h-[630px] pt-[30px] pl-[30px]"
         >
           <video
             ref="videoPlayer"
-            :src="laptop.video"
+            :src="laptop.src"
             class="w-[1100px] cursor-pointer"
             @click="toggleVideo"
             autoplay
