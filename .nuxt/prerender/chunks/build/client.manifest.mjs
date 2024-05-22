3aa622c9f131f38015cdcbf321d988b5209372f5
RexxMK <rikkekodahl@live.dk>
2024-05-22 13:50:58 +0200

    build

---
+++ .nuxt/prerender/chunks/build/client.manifest.mjs
@@ -0,0 +1,178 @@
+const client_manifest = {
+  "_BJKkM7DV.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BJKkM7DV.js",
+    "name": "vue.f36acd1f",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_CgMKbKVn.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CgMKbKVn.js",
+    "name": "ScrollGallery",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_DKepketX.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DKepketX.js",
+    "name": "pageData"
+  },
+  "_DlAUqK2U.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DlAUqK2U.js",
+    "name": "_plugin-vue_export-helper"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "K5Bi7j8h.js",
+    "name": "error-404",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_BJKkM7DV.js",
+      "_DlAUqK2U.js"
+    ],
+    "css": [
+      "error-404.C9l8SaQ8.css"
+    ]
+  },
+  "error-404.C9l8SaQ8.css": {
+    "file": "error-404.C9l8SaQ8.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CqaxiKAF.js",
+    "name": "error-500",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_BJKkM7DV.js",
+      "_DlAUqK2U.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "error-500.CbowB0ry.css"
+    ]
+  },
+  "error-500.CbowB0ry.css": {
+    "file": "error-500.CbowB0ry.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "B9wLIpi0.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "css": [
+      "entry.D8rh1TvX.css"
+    ]
+  },
+  "entry.D8rh1TvX.css": {
+    "file": "entry.D8rh1TvX.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/AfterAugust.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "D9zkGS01.js",
+    "name": "AfterAugust",
+    "src": "pages/AfterAugust.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_CgMKbKVn.js",
+      "_DKepketX.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "pages/NotToScale.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CJYIDeoc.js",
+    "name": "NotToScale",
+    "src": "pages/NotToScale.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_CgMKbKVn.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DKepketX.js",
+      "_DlAUqK2U.js"
+    ],
+    "css": [
+      "NotToScale.I3f1VsYF.css"
+    ]
+  },
+  "NotToScale.I3f1VsYF.css": {
+    "file": "NotToScale.I3f1VsYF.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Cgpi2E6f.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DKepketX.js",
+      "_DlAUqK2U.js"
+    ],
+    "css": [
+      "index.DLmxVxFB.css"
+    ]
+  },
+  "index.DLmxVxFB.css": {
+    "file": "index.DLmxVxFB.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
