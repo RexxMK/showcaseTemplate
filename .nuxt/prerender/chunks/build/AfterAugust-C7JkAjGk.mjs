754e04851afa2775fd18eedbc113bd8dba14e634
RexxMK <rikkekodahl@live.dk>
2024-05-22 14:04:54 +0200

    build

---
+++ .nuxt/prerender/chunks/build/AfterAugust-C7JkAjGk.mjs
@@ -0,0 +1,89 @@
+import { _ as _sfc_main$1$1, a as _sfc_main$2, b as _sfc_main$3, c as _sfc_main$2$1 } from './ScrollGallery-CuzMpPIn.mjs';
+import { ref, mergeProps, unref, useSSRContext } from 'file:///Users/rikke/Documents/GitHub/showcaseTemplate/node_modules/vue/index.mjs';
+import { ssrRenderAttrs, ssrRenderComponent, ssrRenderAttr } from 'file:///Users/rikke/Documents/GitHub/showcaseTemplate/node_modules/vue/server-renderer/index.mjs';
+import { p as pageData } from './pageData-Cu7rdIyB.mjs';
+
+const _sfc_main$1 = {
+  __name: "VideoBorder",
+  __ssrInlineRender: true,
+  props: {
+    laptop: Object,
+    mobile: Object
+  },
+  setup(__props) {
+    ref(null);
+    const isMobile = ref(false);
+    return (_ctx, _push, _parent, _attrs) => {
+      _push(`<div${ssrRenderAttrs(mergeProps({ class: "c-video-border" }, _attrs))}>`);
+      if (unref(isMobile)) {
+        _push(`<div><div class="flex justify-center">`);
+        if (__props.mobile.src) {
+          _push(`<div class="b-solid b-detail"><video${ssrRenderAttr("src", __props.mobile.src)} class="w-[330px] cursor-pointer m-[8px]" autoplay muted loop></video></div>`);
+        } else {
+          _push(`<!---->`);
+        }
+        _push(`</div></div>`);
+      } else {
+        _push(`<div><div class="flex justify-center">`);
+        if (__props.laptop.src) {
+          _push(`<div class="b-solid b-2 b-detail"><video${ssrRenderAttr("src", __props.laptop.src)} class="w-[1100px] cursor-pointer m-[14px]" autoplay muted loop></video></div>`);
+        } else {
+          _push(`<!---->`);
+        }
+        _push(`</div></div>`);
+      }
+      _push(`</div>`);
+    };
+  }
+};
+const _sfc_setup$1 = _sfc_main$1.setup;
+_sfc_main$1.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("components/VideoBorder.vue");
+  return _sfc_setup$1 ? _sfc_setup$1(props, ctx) : void 0;
+};
+const _sfc_main = {
+  __name: "AfterAugust",
+  __ssrInlineRender: true,
+  setup(__props) {
+    const page = ref(pageData.afterAugust);
+    const teaserMaxWidth = ref("max-w-27ch");
+    const isMobile = ref(false);
+    return (_ctx, _push, _parent, _attrs) => {
+      const _component_ImageSlider = _sfc_main$3;
+      const _component_ImageGallery = _sfc_main$2$1;
+      _push(`<div${ssrRenderAttrs(mergeProps({ class: "afterAugust bg-primary" }, _attrs))}>`);
+      if (unref(isMobile)) {
+        _push(`<div class="flex flex-col gap-y-[165px] pb-[165px]">`);
+        _push(ssrRenderComponent(_sfc_main$1$1, mergeProps({ ...unref(page).hero }, { teaserMaxWidth: unref(teaserMaxWidth) }), null, _parent));
+        _push(ssrRenderComponent(_sfc_main$1, { ...unref(page).borderVideo.intro }, null, _parent));
+        _push(ssrRenderComponent(_component_ImageSlider, { ...unref(page).imageSlider }, null, _parent));
+        _push(ssrRenderComponent(_sfc_main$2, { ...unref(page).scrollGallery }, null, _parent));
+        _push(ssrRenderComponent(_sfc_main$1, { ...unref(page).borderVideo.story }, null, _parent));
+        _push(ssrRenderComponent(_component_ImageGallery, mergeProps({ ...unref(page).imageGallery }, { sliceAmount: 2 }), null, _parent));
+        _push(ssrRenderComponent(_sfc_main$1, { ...unref(page).borderVideo.outro }, null, _parent));
+        _push(`</div>`);
+      } else {
+        _push(`<div class="flex flex-col gap-y-[360px] pb-[360px]">`);
+        _push(ssrRenderComponent(_sfc_main$1$1, { ...unref(page).hero }, null, _parent));
+        _push(ssrRenderComponent(_sfc_main$1, { ...unref(page).borderVideo.intro }, null, _parent));
+        _push(ssrRenderComponent(_component_ImageSlider, { ...unref(page).imageSlider }, null, _parent));
+        _push(ssrRenderComponent(_sfc_main$2, { ...unref(page).scrollGallery }, null, _parent));
+        _push(ssrRenderComponent(_sfc_main$1, { ...unref(page).borderVideo.story }, null, _parent));
+        _push(ssrRenderComponent(_component_ImageGallery, mergeProps({ ...unref(page).imageGallery }, { sliceAmount: 3 }), null, _parent));
+        _push(ssrRenderComponent(_sfc_main$1, { ...unref(page).borderVideo.outro }, null, _parent));
+        _push(`</div>`);
+      }
+      _push(`</div>`);
+    };
+  }
+};
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("pages/AfterAugust.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=AfterAugust-C7JkAjGk.mjs.map
---
+++ .nuxt/prerender/chunks/build/client.manifest.mjs
@@ -0,0 +1,178 @@
+const client_manifest = {
+  "_D4ueDy5D.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "D4ueDy5D.js",
+    "name": "vue.f36acd1f",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_DKepketX.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DKepketX.js",
+    "name": "pageData"
+  },
+  "_DlAUqK2U.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DlAUqK2U.js",
+    "name": "_plugin-vue_export-helper"
+  },
+  "_O1jS-TbB.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "O1jS-TbB.js",
+    "name": "ScrollGallery",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CBO1uZro.js",
+    "name": "error-404",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_D4ueDy5D.js",
+      "_DlAUqK2U.js"
+    ],
+    "css": [
+      "error-404.C9l8SaQ8.css"
+    ]
+  },
+  "error-404.C9l8SaQ8.css": {
+    "file": "error-404.C9l8SaQ8.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "yYuwCDPN.js",
+    "name": "error-500",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_D4ueDy5D.js",
+      "_DlAUqK2U.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "error-500.CbowB0ry.css"
+    ]
+  },
+  "error-500.CbowB0ry.css": {
+    "file": "error-500.CbowB0ry.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C4MlAiGu.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "css": [
+      "entry.D8rh1TvX.css"
+    ]
+  },
+  "entry.D8rh1TvX.css": {
+    "file": "entry.D8rh1TvX.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/AfterAugust.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Ch9-EVJh.js",
+    "name": "AfterAugust",
+    "src": "pages/AfterAugust.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_O1jS-TbB.js",
+      "_DKepketX.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "pages/NotToScale.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C5ToewAv.js",
+    "name": "NotToScale",
+    "src": "pages/NotToScale.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_O1jS-TbB.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DKepketX.js",
+      "_DlAUqK2U.js"
+    ],
+    "css": [
+      "NotToScale.I3f1VsYF.css"
+    ]
+  },
+  "NotToScale.I3f1VsYF.css": {
+    "file": "NotToScale.I3f1VsYF.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C3l92bsj.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DKepketX.js",
+      "_DlAUqK2U.js"
+    ],
+    "css": [
+      "index.DLmxVxFB.css"
+    ]
+  },
+  "index.DLmxVxFB.css": {
+    "file": "index.DLmxVxFB.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
